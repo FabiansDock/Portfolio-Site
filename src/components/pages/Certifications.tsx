327f5e1fa5f3ad68c746588316799267a4e89a66
Fabian <romanreigns397@gmail.com>
2024-06-22 22:40:27 +0530

    Built the certifications page

--- src/components/pages/Certifications.tsx
+++ src/components/pages/Certifications.tsx
@@ -1,9 +1,31 @@
-import { Text } from "@chakra-ui/react";
+import { useState } from "react";
+import certificationsFiles from "../service";
+import { Button, HStack, Image } from "@chakra-ui/react";
 
 const Certifications = () => {
+  const [activeImage, setActiveImage] = useState(0);
+
   return (
     <>
-      <Text>Certificates</Text>
+      <HStack>
+        <Button
+          isDisabled={activeImage === 0}
+          onClick={() => setActiveImage(activeImage - 1)}
+        >
+          {"<"}
+        </Button>
+        <Image
+          flex={0.7}
+          src={certificationsFiles[activeImage]}
+          boxSize="50%"
+        />
+        <Button
+          isDisabled={activeImage === certificationsFiles.length - 1}
+          onClick={() => setActiveImage(activeImage + 1)}
+        >
+          {">"}
+        </Button>
+      </HStack>
     </>
   );
 };
---
+++ src/components/service.ts
@@ -0,0 +1,14 @@
+import c1 from "../assets/certifications/c_classes.png";
+import c2 from "../assets/certifications/c_fundamentals.png";
+import d1 from "../assets/certifications/django_part_1.png";
+import d2 from "../assets/certifications/django_part_2.png";
+import d3 from "../assets/certifications/django_part_3.png";
+import docker from "../assets/certifications/docker.png";
+import git from "../assets/certifications/git.png";
+import node from "../assets/certifications/node.png";
+import python from "../assets/certifications/python.png";
+import r1 from "../assets/certifications/react_18_beginners.png";
+
+const certificationsFiles = [c1, c2, d1, d2, d3, docker, git, node, python, r1];
+
+export default certificationsFiles;
