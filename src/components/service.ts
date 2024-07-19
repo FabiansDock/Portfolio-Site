2375879c5afe6af2aa6bbc34e8d8fb4b504069cc
FabiansDock <fabian20122001@gmail.com>
2024-07-19 08:34:16 +0530

    Added nptel certifications

--- src/components/service.ts
+++ src/components/service.ts
@@ -9,6 +9,19 @@ import node from "../assets/certifications/node.png";
 import python from "../assets/certifications/python.png";
 import r1 from "../assets/certifications/react_18_beginners.png";
 
-const certificationsFiles = [c1, c2, d1, d2, d3, docker, git, node, python, r1];
+import nptel_java from "../assets/certifications/programming_in_java.jpg";
+import nptel_datascience from "../assets/certifications/data_science_for_engineers.jpg";
 
-export default certificationsFiles;
+export const certificationsFiles = [
+  c1,
+  c2,
+  d1,
+  d2,
+  d3,
+  docker,
+  git,
+  node,
+  python,
+  r1,
+];
+export const nptel = [nptel_java, nptel_datascience];
