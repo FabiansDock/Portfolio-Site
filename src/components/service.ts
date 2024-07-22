d6a202c24534e5a20de4767617fa5f485fad0f5c
FabiansDock <fabian20122001@gmail.com>
2024-07-22 05:58:03 +0530

    Added c sharp advanced certificate

--- src/components/service.ts
+++ src/components/service.ts
@@ -1,5 +1,6 @@
-import c1 from "../assets/certifications/c_classes.png";
-import c2 from "../assets/certifications/c_fundamentals.png";
+import cs1 from "../assets/certifications/cs_fundamentals.png";
+import cs2 from "../assets/certifications/cs_classes.png";
+import cs3 from "../assets/certifications/cs_advanced.png";
 import d1 from "../assets/certifications/django_part_1.png";
 import d2 from "../assets/certifications/django_part_2.png";
 import d3 from "../assets/certifications/django_part_3.png";
@@ -12,5 +13,5 @@ import r1 from "../assets/certifications/react_18_beginners.png";
 import nptel_java from "../assets/certifications/programming_in_java.jpg";
 import nptel_datascience from "../assets/certifications/data_science_for_engineers.jpg";
 
-export const mosh = [c1, c2, d1, d2, d3, docker, git, node, python, r1];
+export const mosh = [cs1, cs2, cs3, d1, d2, d3, docker, git, node, python, r1];
 export const nptel = [nptel_java, nptel_datascience];
